bb2faa8016fc892e0fbf1c4530798183540e333a
Wiktor00Nedew <wiktor.a.n.20@gmail.com>
2022-09-21 17:28:28 +0200

    sync

--- include/windows/MainWindow.h
+++ include/windows/MainWindow.h
@@ -5,4 +5,22 @@
 #ifndef X_PACE_MAINWINDOW_H
 #define X_PACE_MAINWINDOW_H
 
+#include <QWidget>
+#include <QGridLayout>
+
+class MainWindow : public QWidget{
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+   ~MainWindow();
+
+private:
+    // layouts
+    QGridLayout *mainLayout_;
+
+    // methods
+
+
+};
+
 #endif //X_PACE_MAINWINDOW_H
--- src/windows/MainWindow.cpp
+++ src/windows/MainWindow.cpp
@@ -1,3 +1,16 @@
 //
 // Created by wiktor on 9/21/22.
 //
+
+#include "windows/MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent) {
+    mainLayout_ = new QGridLayout();
+
+    setLayout(mainLayout_);
+    resize(800, 600);
+}
+
+MainWindow::~MainWindow() {
+   delete mainLayout_;
+}
